efd80314f15e520c2bec409a645d67697aad85c2
Stefan Fercot <stefan.fercot@enterprisedb.com>
2020-10-23 14:51:27 +0200

    add azurite test case

--- tests/centos7/provision/pgbackrest_azurite_primary.bash
+++ tests/centos7/provision/pgbackrest_azurite_primary.bash
@@ -17,17 +17,20 @@ if [ $ENCRYPTED = "true" ]; then
 repo1-cipher-pass=acbd'
 fi
 
+HOST_AZURE="backup-srv"
+HOST_AZURE_ACCOUNT="pgbackrest"
+HOST_AZURE_KEY="YXpLZXk="
+HOST_AZURE_CONTAINER="pgbackrest-container"
+
 cat<<EOC > "/etc/pgbackrest.conf"
 [global]
+repo1-type=azure
+repo1-azure-host=$HOST_AZURE
+repo1-azure-verify-tls=n
+repo1-azure-account=$HOST_AZURE_ACCOUNT
+repo1-azure-key=$HOST_AZURE_KEY
+repo1-azure-container=$HOST_AZURE_CONTAINER
 repo1-path=/repo1
-repo1-type=s3
-repo1-s3-endpoint=minio.local
-repo1-s3-bucket=pgbackrest
-repo1-s3-verify-tls=n
-repo1-s3-key=accessKey
-repo1-s3-key-secret=superSECRETkey
-repo1-s3-region=eu-west-3
-
 repo1-retention-full=1
 process-max=2
 log-level-console=warn
@@ -42,13 +45,6 @@ pg1-user=${PGUSER}
 pg1-port=${PGPORT}
 EOC
 
-# setup DNS alias for backup-srv where minio is installed
-MINIO_IP=`getent hosts backup-srv | awk '{ print $1 }'`
-cat<<EOF >>"/etc/hosts"
-$MINIO_IP pgbackrest.minio.local minio.local s3.eu-west-3.amazonaws.com
-EOF
-
-# create stanza
 sudo -iu ${PGUSER} pgbackrest --stanza=my_stanza stanza-create
 
 # archive_command setup
@@ -58,7 +54,5 @@ SELECT pg_reload_conf();
 EOS
 
 sudo -iu ${PGUSER} pgbackrest --stanza=my_stanza check
-
-# install perl modules needed for check_pgbackrest S3 compatibility
-yum install --nogpgcheck --quiet -y -e 0 http://repo.openfusion.net/centos7-x86_64/openfusion-release-0.7-1.of.el7.noarch.rpm
-yum install  --nogpgcheck --quiet -y -e 0 perl-Config-IniFiles perl-Net-Amazon-S3
+sudo -iu ${PGUSER} pgbackrest --stanza=my_stanza backup --type=full --repo1-retention-full=1
+sudo -iu ${PGUSER} pgbackrest --stanza=my_stanza info
